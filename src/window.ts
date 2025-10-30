59414ae08968d6a3db71de355cc184f23b89522a
Jonas Alves <zeropior@proton.me>
2025-10-30 19:39:01 -0300

    update userAgent

--- src/window.ts
+++ src/window.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { URL } from 'url';
 
 const userAgent =
-  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36';
+  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36';
 
 function createBrowserWindowIcon(): NativeImage {
   const basePath = app.isPackaged ? process.resourcesPath : app.getAppPath();
